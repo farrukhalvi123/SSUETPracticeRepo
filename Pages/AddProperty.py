7c0e06b205cb13dbfd068d62580665282bdb857f
Muhammad Mooazam <muhammadmoazam29@gmail.com>
2022-12-24 15:19:25 +0500

    second Commit

---
+++ Pages/AddProperty.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+
+class AddProperty:
+    def __int__(self, driver):
+        self.driver = driver
+        self.addproperty = "//span[normalize-space()='Post Property FREE']"
+        self.rent = "//div[@id='manage_property']//li[@class='MuiListItem-root MuiListItem-gutters MuiListItem-padding css-1yo8bqd']"
+        self.highrise = "//span[@class='icon-apartment']"
+        self.propertyname = "propertyName"
+
+    def click_addproperty(self):
+        self.driver.find_element(By.XPATH, self.addproperty).click()
+
+    def click_rent(self):
+        self.driver.find_element(By.XPATH, self.rent).click()
+
+    def enter_propertyname(self, prop):
+        self.driver.find_element(By.ID, self.propertyname).send_keys(prop)
--- Test/SpeedhomeTests.py
+++ Test/SpeedhomeTests.py
@@ -3,6 +3,8 @@ import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+from Pages.AddProperty import AddProperty
+
 
 class SpeedHomeTest(unittest.TestCase):
     @classmethod
@@ -10,6 +12,13 @@ class SpeedHomeTest(unittest.TestCase):
         cls.driver = webdriver.Chrome(ChromeDriverManager().install())
         cls.driver.get("beta.speedhome.com")
         cls.driver.maximize_window()
+
+    def test_addproperty(self):
+        driver = self.driver
+        add = AddProperty(driver)
+        add.click_addproperty()
+        add.click_rent()
+        add.enter_propertyname("abcxyz")
 
     def tearDownClass(cls):
         cls.driver.quit()
